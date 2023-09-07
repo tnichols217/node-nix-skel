866b993cb6029f9b8851deb83f07649db055d0da
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-09-06 22:42:29 -0400

    update and test builds

--- flake.nix
+++ flake.nix
@@ -7,67 +7,70 @@
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dream2nix.url = "github:nix-community/dream2nix/legacy";
+    dream2nix.url = "github:nix-community/dream2nix";
   };
 
   outputs = { self, nixpkgs, flake-utils, dream2nix, gitignore }:
     let
-      nixpkgs = import dream2nix.inputs.nixpkgs {};
-      lib = nixpkgs.lib;
 
-      _callModule = module:
-        nixpkgs.lib.evalModules {
-          specialArgs.dream2nix = dream2nix;
-          specialArgs.packageSets.nixpkgs = nixpkgs;
-          modules = [module ./settings.nix dream2nix.modules.dream2nix.core];
-        };
 
-      # like callPackage for modules
-      callModule = module: (_callModule module).config.public;
+      customOut = flake-utils.lib.eachDefaultSystem (system:
+        let
+          name = "node-nix-skel";
+          pkgs = nixpkgs.legacyPackages.${system};
 
-      packageModuleNames = builtins.attrNames (builtins.readDir ./packages);
+          _callModule = module:
+            nixpkgs.lib.evalModules {
+              specialArgs = {
+                inherit dream2nix;
+                packageSets.nixpkgs = import dream2nix.inputs.nixpkgs { inherit system; };
+              };
+              modules = [module ./settings.nix dream2nix.modules.dream2nix.core];
+            };
 
-      packages =
-        lib.genAttrs packageModuleNames
-        (moduleName: callModule "${./packages}/${moduleName}/module.nix");
+          # like callPackage for modules
+          callModule = module: (_callModule module).config.public;
 
-      dream2nixOut = {
-        packages = packages;
-      };
+          packageModuleNames = builtins.attrNames (builtins.readDir ./packages);
 
-      # customOut = flake-utils.lib.eachDefaultSystem (system:
-      #   let
-      #     name = "node-nix-skel";
-      #     pkgs = nixpkgs.legacyPackages.${system};
-      #     app = dream2nixOut.packages."${system}"."${name}";
-      #   in with pkgs; {
-      #     packages = rec {
-      #       filtered = pkgs.callPackage ./nix/filter.pkg.nix { file = app; inherit name; };
-      #       docker = pkgs.callPackage ./nix/docker.pkg.nix { app = filtered; inherit name; };
-      #       node = app;
-      #       default = filtered;
-      #     };
-      #     apps = rec {
-      #       dev = {
-      #         type = "app";
-      #         program = ./nix/scripts/dev.sh;
-      #       };
-      #       devProd = {
-      #         type = "app";
-      #         program = ./nix/scripts/devProd.sh;
-      #       };
-      #       start = {
-      #         type = "app";
-      #         program = ./nix/scripts/start.sh;
-      #       };
-      #       build = {
-      #         type = "app";
-      #         program = ./nix/scripts/build.sh;
-      #       };
-      #       default = dev;
-      #     };
-      #   });
+          packages =
+            nixpkgs.lib.genAttrs packageModuleNames
+            (moduleName: callModule "${./packages}/${moduleName}/module.nix");
+
+          dream2nixOut = {
+            inherit packages;
+          };
+
+        in with pkgs; {
+          packages = rec {
+            ${name} = packages.${name};
+            filtered = pkgs.callPackage ./nix/filter.pkg.nix { file = packages.${name}; inherit name; };
+            docker = pkgs.callPackage ./nix/docker.pkg.nix { app = filtered; inherit name; };
+            node = packages.${name};
+            default = node;
+          };
+          apps = rec {
+            dev = {
+              type = "app";
+              program = ./nix/scripts/dev.sh;
+            };
+            devProd = {
+              type = "app";
+              program = ./nix/scripts/devProd.sh;
+            };
+            start = {
+              type = "app";
+              program = ./nix/scripts/start.sh;
+            };
+            build = {
+              type = "app";
+              program = ./nix/scripts/build.sh;
+            };
+            default = dev;
+          };
+        });
     in
-    dream2nixOut;
+    # dream2nixOut;
     # nixpkgs.lib.recursiveUpdate dream2nixOut customOut;
+    customOut;
 }
---
+++ packages/node-nix-skel/module.nix
@@ -0,0 +1,27 @@
+{
+  dream2nix,
+  config,
+  lib,
+  nixpkgs,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-lock
+    dream2nix.modules.dream2nix.nodejs-granular
+  ];
+
+  name = "node-nix-skel";
+  version = "1.0.0";
+
+  deps = {nixpkgs, ...}: {
+    stdenv = nixpkgs.stdenv;
+    # inherit
+    #   (nixpkgs)
+    #   stdenv
+    #   ;
+  };
+
+  mkDerivation = {
+    src = ./.;
+  };
+}
